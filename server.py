985bea0fb093442bbde931a5b6a5a7e0ef479ba9
Sureinity <johnghlendealdo@gmail.com>
2025-09-13 15:09:29 +0800

    configure network

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
+import os
 from textual_serve.server import Server
 
-server = Server("python portfolio")
+port = int(os.environ.get("PORT", 8000))
+host = "0.0.0.0"
+
+server = Server("python portfolio.py", port=port, host=host)
 server.serve()
