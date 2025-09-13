50d5bbeeea016139ea68286068cc982167b55b62
Sureinity <johnghlendealdo@gmail.com>
2025-09-13 15:21:12 +0800

    add public url

--- server.py
+++ server.py
@@ -4,5 +4,10 @@ from textual_serve.server import Server
 port = int(os.environ.get("PORT", 10000))
 host = "0.0.0.0"
 
-server = Server("python portfolio.py", port=port, host=host)
+server = Server(
+    command="python portfolio.py", 
+    port=port,
+    host=host,
+    public_url="https://portfolio-pzey.onrender.com"
+)
 server.serve()
