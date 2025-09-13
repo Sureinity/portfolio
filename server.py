b36a89fb11c12a9de83d0c036b7c5e704fbf41e7
Sureinity <johnghlendealdo@gmail.com>
2025-09-13 15:14:49 +0800

    change port

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
 from textual_serve.server import Server
 
-port = int(os.environ.get("PORT", 8000))
+port = int(os.environ.get("PORT", 10000))
 host = "0.0.0.0"
 
 server = Server("python portfolio.py", port=port, host=host)
